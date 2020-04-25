6a2b33da3c6cf16763c421ef77f4f1b805399dde
Zviad Mgaloblishvili <hausiusi@gmail.com>
2020-04-26 01:30:27 +0200

    Implement system performance monitor
    Task manager thread is redesigned all common useful code moved to
    mgr_tasks.
    Get performance info implementation collects information about system
    performance. These values are:
    - Core clock speed
    - APB1 clock speed
    - APB2 clock speed
    - AHB clock speed
    - Available memory in OS heap (is not real physical memory)
    - Currently running tasks count
    - Average CPU utilization (all time)
    - Total uptime
    
    All this functionality can be retrieved using `perfinfo` command from
    cmdline.
    
    Issue-id: #5

--- System/Debugging/Src/performance.c
+++ System/Debugging/Src/performance.c
@@ -5,7 +5,14 @@
  *      Author: Zviad
  */
 
+#include <string.h>
 #include "performance.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_rcc.h"
+#include "FreeRTOS.h"
+#include "mw_timebase.h"
+#include "mgr_tasks.h"
 
 static TIM_HandleTypeDef htim5, htim4;
 
@@ -34,6 +41,30 @@ inline uint32_t prf_end_measurement() {
 	HAL_TIM_Base_Stop(&htim5);
 	__HAL_RCC_TIM5_CLK_DISABLE();
 	return cnt;
+}
+
+void prf_get_info(perfinfo_t* perfinfo) {
+	perfinfo->core_clock = HAL_RCC_GetSysClockFreq();
+	perfinfo->apb1_clock = HAL_RCC_GetPCLK1Freq();
+	perfinfo->apb2_clock = HAL_RCC_GetPCLK2Freq();
+	perfinfo->ahb_clock = HAL_RCC_GetHCLKFreq();
+	perfinfo->available_heap = xPortGetFreeHeapSize();
+	tasks_statuses_t* tasks_statuses = mgr_tasks_get_system_state();
+	perfinfo->tasks_count = tasks_statuses->current_count;
+	if (tasks_statuses->is_available) {
+		uint32_t total_runtime = 0, idle_runtime = 0;
+		for (int i = 0; i < tasks_statuses->current_count; i++) {
+			total_runtime += tasks_statuses->current[i].ulRunTimeCounter;
+			if (!strncmp(tasks_statuses->current[i].pcTaskName, "IDLE", 4)) {
+				idle_runtime = tasks_statuses->current[i].ulRunTimeCounter;
+			}
+		}
+		total_runtime /= 100;
+		if (total_runtime) {
+			perfinfo->cpu_utilization = 100 - (idle_runtime / total_runtime);
+		}
+	}
+	perfinfo->uptime = mw_timebase_ticks_get();
 }
 
 static void _timer_measurer_init() {
