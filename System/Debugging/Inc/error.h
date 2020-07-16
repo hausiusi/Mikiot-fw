e18f9ff1a94d63e8f3e01d478a95fdb03772d9e0
Zviad Mgaloblishvili <hausiusi@gmail.com>
2020-07-16 22:41:47 +0200

    Adjust error reporting to new config
    - error_report logs in user interface always when unit tests are not
    compiled. When unit tests are compiled report will be logged to test
    framework using hook
    - Instead of number now enumerator name will be logged in report for
    better readability purposes.
    
    Issue-id: #93

--- System/Debugging/Inc/error.h
+++ System/Debugging/Inc/error.h
@@ -9,10 +9,11 @@
 #define ERROR_H_
 
 #include "debug.h"
+#include "mikiot_config.h"
 
 /* Get file path, line and function name where error happened, using
  * __FILE__, __LINE__ and __FUNCTION__ macro */
-#define error_report(id, type) (error_report_(__FILE__, __LINE__, __FUNCTION__, id, type))
+#define error_report(id, type) (mikiot_error_report(id, type))
 // @formatter:off
 typedef enum {
 	InitError = 0,
@@ -31,6 +32,6 @@ typedef struct {
 } error_t;
 
 void error_report_(const char* file_name, size_t line, const char* func_name,
-		uint32_t id, uint32_t type);
+		uint32_t id, const char* type);
 
 #endif /* ERROR_H_ */
