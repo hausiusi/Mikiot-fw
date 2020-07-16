aba11908c90efe5f9c2c37329f4be15a3cee2197
Zviad Mgaloblishvili <hausiusi@gmail.com>
2020-07-16 22:41:47 +0200

    Adjust error reporting to new config
    - error_report logs in user interface always when unit tests are not
    compiled. When unit tests are compiled report will be logged to test
    framework using hook
    - Instead of number now enumerator name will be logged in report for
    better readability purposes.
    
    Issue-id: #93

--- System/Debugging/Src/error.c
+++ System/Debugging/Src/error.c
@@ -12,8 +12,8 @@
 #include "task.h"
 
 void error_report_(const char* file_name, size_t line, const char* func_name,
-		uint32_t id, uint32_t type) {
-	debug_error("%u\nType: %u\nFile: %s\nLine: %i\nFunction: %s\n", id, type,
+		uint32_t id, const char* type) {
+	debug_error("%u\nType: %s\nFile: %s\nLine: %i\nFunction: %s\n", id, type,
 			file_name, line, func_name);
 }
 
