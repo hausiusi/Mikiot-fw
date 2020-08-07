742ff2a01f0353edcc2bcfb9dca3324130fade4a
Zviad Mgaloblishvili <hausiusi@gmail.com>
2020-08-08 00:07:05 +0200

    ioconfig: define adc1 in1 gpio
    Issue-id: #115

--- Core/Inc/ioconfig.h
+++ Core/Inc/ioconfig.h
@@ -10,6 +10,7 @@
 #ifndef INC_IOCONFIG_H_
 #define INC_IOCONFIG_H_
 
+/* GPIO */
 #define BTN1_GPIO GPIOA
 #define BTN1_GPIO_PIN GPIO_PIN_0
 
@@ -18,5 +19,9 @@
 
 #define BTN3_GPIO GPIOB
 #define BTN3_GPIO_PIN GPIO_PIN_2
+
+/* ADC */
+#define ADC1_IN1_GPIO          GPIOB
+#define ADC1_IN1_GPIO_PIN      GPIO_PIN_0
 
 #endif /* INC_IOCONFIG_H_ */
